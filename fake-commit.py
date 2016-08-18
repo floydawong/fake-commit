ab8a782b81e92be3f312c04d60fd5834d10c5b53
Floyda <floyda@163.com>
2016-08-19 01:51:58 +0800

    update

--- fake-commit.py
+++ fake-commit.py
@@ -1,11 +1,14 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
+# Author: Floyda
 
 import github3
-import time
+import time as stime
 import datetime
 import random
 
+DEBUG = True
+MAX_RANGE = 10
 gh = None
 
 
@@ -15,75 +18,99 @@ class Config:
     fake_repo_name = "fake-commit-log"
 
 
-def debug(msg):
-    print(msg)
 
+def debug_log(msg):
+    print "[Info]", msg
+
+
+def error_log(msg):
+    print "[Error]", msg
+    exit()
 
 def get_github_date():
     us_time = datetime.datetime.utcnow()
-    # debug(us_time)
     return str(us_time.date())
 
 
 def has_commit_today():
     date = get_github_date()
+    repos = github3.iter_user_repos(Config.user_name)
     try:
-        repos = github3.iter_user_repos(Config.user_name)
-    except Exception, e:
-        raise e
+        for repo in repos:
+            if date in str(repo.pushed_at):
+                return True
+    except:
+        error_log("Cann't search User: %s" % Config.user_name)
 
-    for repo in repos:
-        # debug(repo.pushed_at, repo.name)
-        if date in str(repo.pushed_at):
-            return True
     return False
 
 
-def get_fake_repo():
-    global gh
+def find_fake_repo(time=1):
+    if time >= 10: return None
     try:
+        global gh
         repo = gh.repository(Config.user_name, Config.fake_repo_name)
-    except Exception, e:
-        raise e
+    except:
+        debug_log("find_fake_repo ... %d" % time)
+        return find_fake_repo(time + 1)
+    return repo
 
+
+def create_fake_repo(time=1):
+    if time >= 10: return None
+    try:
+        repo = gh.create_repo(Config.fake_repo_name)
+    except:
+        debug_log("create_fake_repo ... %d" % time)
+        return create_fake_repo(time + 1)
+    return repo
+
+
+def get_fake_repo():
+    repo = find_fake_repo()
     if repo:
         return repo
     else:
-        try:
-            new_repo = gh.create_repo(Config.fake_repo_name)
-        except Exception, e:
-            raise e
-        return new_repo
+        return create_fake_repo()
 
 
-def fake_commit():
+def fake_commit(time=1, failed_total=0):
+    if failed_total > 12:
+        error_log("Commit limit exceeded")
+    if time == 0:
+        debug_log("Fake Commit Finish")
+        return
+
     repo = get_fake_repo()
     if repo is None:
-        debug('Repo Is None')
-    else:
-        debug(repo)
-    timestamp = get_github_date() + "-" + str(datetime.datetime.utcnow().time())[:5]
+        debug_log("Cann't get repository")
+        fake_commit(time, failed_total + 1)
 
+    timestamp = get_github_date() + "-" + str(datetime.datetime.utcnow().time())[:5]
     path = "log/%s" % timestamp
     msg = "hasnt commit at %s" % timestamp
     content = timestamp
     try:
         repo.create_file(path, msg, content)
-    except Exception, e:
-        raise e
-    debug('Fake Push is OK')
+    except:
+        debug_log("... %d" % failed_total)
+        stime.sleep(5)
+        fake_commit(time, failed_total + 1)
+        return
+
+    debug_log("fake commit successful ... %d" % time)
+    fake_commit(time - 1)
 
 
 def main():
+    debug_log(datetime.datetime.utcnow())
+    debug_log("Fake Commit Began ...")
     global gh
     gh = github3.login(Config.user_name, password=Config.user_passwd)
-    debug('Name : {}'.format(Config.user_name))
-    if not has_commit_today():
-        debug('hasnt push today ... ')
-        for x in xrange(random.randint(2, 10)):
-            fake_commit()
+    if DEBUG or not has_commit_today():
+        fake_commit(random.randint(1, MAX_RANGE))
     else:
-        debug('You has pushed today ...')
+        debug_log('You has pushed today ...')
 
 
 if __name__ == '__main__':
